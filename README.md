75032fc79d1e10cbf24f2ec5eca476ab44b71f9c
Arian Bakh <arian@cafebazaar.ir>
2019-05-17 19:16:04 +0430

    Speed up + Complete README.md

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Getting Started
 
 `python3 run.py`
+
+# Results
+
+![](out.png)
+
+Note that this image was generated using only 20k nodes, as 1m nodes would take too long.
+
+# Analysis of Results
+
+From the log-log plot, it is obvious that the degree distribution follows Pareto distribution.
--- run.py
+++ run.py
@@ -1,18 +1,22 @@
 import math
 import matplotlib.pyplot as plt
 import networkx as nx
+import os
 import random
 import sys
 
 
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+OUTPUT_PATH = os.path.join(BASE_DIR, 'out.png')
 GAMMA = 0.5
-NUMBER_OF_NODES = 1000
+NUMBER_OF_NODES = 20000
 
 
 def run():
     graph = nx.DiGraph()
     graph.add_nodes_from([0, 1, 2, 3])
     graph.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 0)])
+
     for i in range(4, NUMBER_OF_NODES):
         if i % 100 == 0:
             sys.stdout.write('\r[%d]' % i)
@@ -25,19 +29,14 @@ def run():
                 random.choice(list(range(i)))
             )
         else:
-            number_of_edges = graph.number_of_edges()
-            sorted_node_degrees = sorted(
-                [(node_id, graph.in_degree(node_id) / number_of_edges) for node_id in graph.nodes()],
-                key=lambda x: -x[1]
-            )
-            sum = 0
-            index = 0
+            number_of_edges = i
             random_number = random.random()
-            while sum < random_number:
-                sum += sorted_node_degrees[index][1]
-                index += 1
-            selected_node_id = sorted_node_degrees[index][0]
-            graph.add_edge(i, selected_node_id)
+            sum = 0
+            node_id = 0
+            while sum / number_of_edges < random_number:
+                sum += graph.in_degree(node_id)
+                node_id += 1
+            graph.add_edge(i, node_id)
     print()  # newline
 
     degree_count = {}
@@ -48,12 +47,12 @@ def run():
         else:
             degree_count[node_degree] = 1
     sorted_degree_count = sorted(degree_count.items(), key=lambda item: -item[0])
-    x = [math.log2(max(1, item[0])) for item in sorted_degree_count]
-    y = [math.log2(max(1, item[1])) for item in sorted_degree_count]
+    x = [math.log10(max(1, item[0])) for item in sorted_degree_count]
+    y = [math.log10(max(1, item[1])) for item in sorted_degree_count]
     plt.plot(x, y)
-    plt.xlabel('log of degree')
-    plt.ylabel('log of count')
-    plt.show()
+    plt.xlabel('log10 of degree')
+    plt.ylabel('log10 of count')
+    plt.savefig(OUTPUT_PATH)
 
 
 if __name__ == '__main__':
